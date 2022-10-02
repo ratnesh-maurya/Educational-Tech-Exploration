a9dd43c25a68f6ffc85a2374f75b84c527db2640
Vinamra300 <73526139+Vinamra300@users.noreply.github.com>
2022-10-02 13:03:59 +0530

    Create AsciiVal.cpp

---
+++ C++/AsciiVal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ /*Program By Ghanendra Yadav
+   Visit http://www.programmingwithbasics.com/
+  */
+char a;
+int b;
+cout<<"Enter The Character ";
+cin>>a;
+b=a;
+cout<<b<<endl;
+}
