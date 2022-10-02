138a7bd6f88357e8352dd195a246afd83f3f0d76
Vinamra300 <73526139+Vinamra300@users.noreply.github.com>
2022-10-02 13:10:36 +0530

    Create Size_of_datatype.cpp

---
+++ C++/Size_of_datatype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+ cout<<"size of int "<<sizeof(int)<<endl;
+ cout<<"size of float "<<sizeof(float)<<endl;
+ cout<<"size of double "<<sizeof(double)<<endl;
+ cout<<"size of long "<<sizeof(long)<<endl;
+ cout<<"size of long double "<<sizeof(long double)<<endl;
+ cout<<"size of long long int "<<sizeof(long long int)<<endl;
+ cout<<"size of long long "<<sizeof(long long)<<endl;
+}
