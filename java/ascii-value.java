a24e100b3ee32ebf1d243842650cf00d365ed98e
Nivesh <59831140+Niveshpai@users.noreply.github.com>
2022-10-03 23:06:24 +0530

    Create ascii-value.java

---
+++ java/ascii-value.java
@@ -0,0 +1,13 @@
+//Code to print ascii value of a character
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
