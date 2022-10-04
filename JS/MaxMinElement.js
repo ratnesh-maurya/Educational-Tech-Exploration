dc14b4b4782c84fbf611a846721072b57f8e9586
Shivangi <65915506+shivangi11012000@users.noreply.github.com>
2022-10-05 01:19:14 +0530

    Maximum and Minimum element in array

---
+++ JS/MaxMinElement.js
@@ -0,0 +1,6 @@
+//FINDING THE MAXIMUM AND MINIMUM ELEMENT IN ARRAY
+
+array = [50, 60, 20, 10, 40];
+minValue = Math.min(...array);
+maxValue = Math.max(...array);
+console.log(minValue,maxValue) //10 60
