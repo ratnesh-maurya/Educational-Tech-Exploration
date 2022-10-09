1cf05ae39ff89ee6e73487eb65cbc60e016cddcb
Asmi Jena <78552982+Asmi-1@users.noreply.github.com>
2022-10-09 18:26:13 +0530

    Parking space counter
    This is a small python file which solves the issues related to parking space.

---
+++ python/Parking_space_counter.py
@@ -0,0 +1,69 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+import tkinter
+import tkinter.messagebox
+from PIL import ImageTk, Image
+print("This is SS MP")
+
+
+# Video feed
+cap = cv2.VideoCapture('carPark.mp4')
+
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+
+width, height = 107, 48
+
+
+def checkParkingSpace(imgPro):
+    spaceCounter = 0
+    idx = 0
+    spacesOcupied = set()
+    for pos in posList:
+        x, y = pos
+
+        imgCrop = imgPro[y:y + height, x:x + width]
+        # cv2.imshow(str(x * y), imgCrop)
+        count = cv2.countNonZero(imgCrop)
+
+        if count < 900:
+            color = (0, 255, 0)
+            thickness = 5
+            spaceCounter += 1
+            spacesOcupied.add(idx)
+        else:
+            color = (0, 0, 255)
+            thickness = 2
+            spacesOcupied.discard(idx)
+        idx += 1
+        cv2.rectangle(img, pos, (pos[0] + width,
+                    pos[1] + height), color, thickness)
+        cvzone.putTextRect(img, str(count), (x, y + height - 3), scale=1,
+                        thickness=2, offset=0, colorR=color)
+    print(spacesOcupied)
+    cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (100, 50), scale=3,  # space count
+                    thickness=5, offset=20, colorR=(0, 200, 0))
+
+
+while True:
+
+    # when avideo end again it start
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    success, img = cap.read()
+    # fast convert image to binary image or gray
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,  # binary image
+                                        cv2.THRESH_BINARY_INV, 25, 16)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+
+    checkParkingSpace(imgDilate)
+    cv2.imshow("Image", img)
+    # cv2.imshow("ImageBlur", imgBlur)
+    # cv2.imshow("ImageThres", imgMedian)
+    cv2.waitKey(10)
