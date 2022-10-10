82b9fcdc1254a93b7eccc0749c5cfb28e0a04b76
Abhishek Rana <abhishekrana21092003@gmail.com>
2022-10-10 18:39:08 +0530

    Added palindrome.py

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,15 +1,15 @@
 #Program to check whether the input is Palindrome or not
 
-#Accepting the Input
+#Accepting the input
 theinput = input("Enter the input: ")
 
-#Reversing the Input
+#Reversing the input
 reversedinput = theinput[::-1]
 
 #Checking if the reversed input is equal to the original input
 if(reversedinput == theinput):
-    #Both are Equal
+    #Both are equal
     print("Palindrome")
 else:
-    #Both are not Equal
+    #Both are not equal
     print("Not Palindrome")
