ca9eadd886a33ab02d80c60d16426d257aa35686
Kshitiz Raj <kshitiz2228@gmail.com>
2022-10-25 12:32:05 +0530

    Added Promodoro clock website

---
+++ Pomodoro Clock/assets/js/progress.js
@@ -0,0 +1,12 @@
+const circle = document.querySelector(".progress-ring__circle");
+const radius = circle.r.baseVal.value;
+const circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = circumference;
+circle.style.strokeDashoffset = circumference;
+
+function setProgress(percent) {
+  const offset = circumference - (percent / 100) * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+
